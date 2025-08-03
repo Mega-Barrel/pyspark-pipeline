c07d1ced360c18f3f2e9ac8267d3be3740b59f73
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2025-08-03 22:10:36 +0530

    refactored file, added util Class imports

--- ecommerce_pipeline/base/__init__.py
+++ ecommerce_pipeline/base/__init__.py
@@ -1,23 +1,5 @@
 
-import abc
-from pyspark.sql import SparkSession, DataFrame
+from .spark_utils import PySparkJobInterface
+from .mongo_utils import MongoConnector
 
-class PySparkJobInterface(abc.ABC):
-    """ Abstract Class"""
-
-    def __init__(self):
-        self.spark = self.init_spark_session()
-
-    @abc.abstractmethod
-    def init_spark_session(self) -> SparkSession:
-        """Create Spark Session"""
-        return NotImplementedError
-
-    # pylint: disable=E1101
-    def read_csv(self, input_path: str) -> DataFrame:
-        """ Read CSV file """
-        return self.spark.read.options(header=True, inferSchema=True).csv(input_path)
-
-    def stop(self) -> None:
-        """ Terminate Spark Session """
-        self.spark.stop()
+__all__ = ['PySparkJobInterface', 'MongoConnector']
