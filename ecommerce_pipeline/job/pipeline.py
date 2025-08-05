f4a1854d8b89df7fe0894f0a6218461114407ac1
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2025-08-05 22:32:18 +0530

    Initial commit

--- ecommerce_pipeline/job/pipeline.py
+++ ecommerce_pipeline/job/pipeline.py
@@ -1,9 +1,25 @@
 
-from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.functions import col, count
-from pyspark.sql.types import IntegerType, FloatType, StringType, StructType, StructField
+from ecommerce_pipeline.base import PySparkJobInterface
 
-class PySparkJob:
+class PySparkJob(PySparkJobInterface):
 
-    def init_spark_session(self) -> SparkSession:
-        return SparkSession.builder.appName("Ecommerce Data Pipeline").getOrCreate()
+    def __init__(self):
+        super().__init__()
+
+    def extract_data(self):
+        identifier_df = self.read_csv(input_path='data/identifies.csv')
+        tracks_df = self.read_csv(input_path='data/tracks.csv')
+        successfull_orders_df = self.read_csv(input_path='data/order_completed.csv')
+        pages_df = self.read_csv(input_path='data/pages.csv')
+
+        return (
+            identifier_df,
+            tracks_df,
+            successfull_orders_df,
+            pages_df
+        )
+
+    def run(self):
+        print("Reading data from files..")
+        data = self.extract_data()
+        self.show_data(data[0])
