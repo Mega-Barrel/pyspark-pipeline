33510d49e1f68d1a1dfe7494eaa10b2b711ab9a3
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2025-08-10 21:54:39 +0530

    added dim_user_df method -> data processing

--- main.py
+++ main.py
@@ -2,8 +2,25 @@
 from ecommerce_pipeline.job.pipeline import PySparkJob
 
 if __name__ == "__main__":
+
     job = PySparkJob()
     try:
-        job.run()
+        print("<<Reading Data>>")
+        identifiers_df = job.read_csv(input_path="data/identifies.csv")   # or "data/identifiers.csv"
+        tracks_df      = job.read_csv(input_path="data/tracks.csv")
+        orders_df      = job.read_csv(input_path="data/order_completed.csv")
+        pages_df       = job.read_csv(input_path="data/pages.csv")
+
+        print("<<Transformation>>")
+
+        dim_user_df = job.dim_users(
+            identifiers_df,
+            tracks_df,
+            orders_df
+        )
+        dim_user_df[0].show(10, truncate=False)
+        print()
+        dim_user_df[1].show(10, truncate=False)
+
     finally:
         job.stop()
