55dac1bb119075eaacf3e5b69e5e909a0a2a26ba
Saurabh Joshi <saurabhjoshi7869@gmail.com>
2025-08-13 22:33:59 +0530

    GCS bucket testing

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
 
 import time
+
+import pandas as pd
 from ecommerce_pipeline.job.pipeline import PySparkJob
+from ecommerce_pipeline.base.gcs_utils import GcsStorageManager
 
 if __name__ == "__main__":
 
-    job = PySparkJob()
-    try:
-        # print(job.get_spark_version())
-        print("<<Reading Data>>")
-        identifiers_df = job.read_csv(input_path="data/identifies.csv")   # or "data/identifiers.csv"
-        tracks_df      = job.read_csv(input_path="data/tracks.csv")
-        orders_df      = job.read_csv(input_path="data/order_completed.csv")
-        pages_df       = job.read_csv(input_path="data/pages.csv")
+    BUCKET_NAME = "ecommerce_pipeline"
+    CREDENTIALS_FILE = "credentials.json"
+    LOCAL_CSV_FILE = "data/identifies.csv"
+    CSV_NAME = "test-data/sample.csv"
 
-        print("<<Transformation>>")
-        df = job.transform(
-            pages_df = pages_df,
-            tracks_df = tracks_df,
-            orders_df = orders_df
-            # write = True
-        )
+    manager = GcsStorageManager(credentials_path=CREDENTIALS_FILE)
+    manager.create_bucket(BUCKET_NAME)
+    manager.upload_file(BUCKET_NAME, str(LOCAL_CSV_FILE), CSV_NAME)
 
-        print("<<Load Data>>")
-        df.show(n=10, truncate=False)
+    identifiers_df = manager.read_csv_from_blob(BUCKET_NAME, CSV_NAME, pd=pd)
+    if identifiers_df is not None:
+        print("\nDataFrame Head:")
+        print(identifiers_df.head())
 
-    finally:
-        time.sleep(10)
-        job.stop()
+    # job = PySparkJob()
+    # try:
+    #     # print(job.get_spark_version())
+    #     print("<<Reading Data>>")
+    #     identifiers_df = job.read_csv(input_path="data/identifies.csv")   # or "data/identifiers.csv"
+    #     tracks_df      = job.read_csv(input_path="data/tracks.csv")
+    #     orders_df      = job.read_csv(input_path="data/order_completed.csv")
+    #     pages_df       = job.read_csv(input_path="data/pages.csv")
+
+    #     print("<<Transformation>>")
+    #     df = job.transform(
+    #         pages_df = pages_df,
+    #         tracks_df = tracks_df,
+    #         orders_df = orders_df
+    #         # write = True
+    #     )
+
+    #     print("<<Load Data>>")
+    #     df.show(n=10, truncate=False)
+
+    # finally:
+    #     time.sleep(10)
+    #     job.stop()
